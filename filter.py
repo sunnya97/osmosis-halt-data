bf443be8523cc03d2bc260d17e1d78291d8d020c
Sunny Aggarwal <sunnya97@protonmail.ch>
2022-08-17 23:49:41 +0900

    remainings and filters

--- filter.py
+++ filter.py
@@ -1,4 +1,3 @@
-import csv
 import json
 import math
 
@@ -14,25 +13,27 @@ for denom in denoms:
         total = 0
 
         filtered_balances = []
+        unincluded_balances = []
 
         for entry in entries:
             total += 1
             # print(float(entry['amount']) * denoms[denom][2] )
             if float(entry['amount']) / math.pow(10, denoms[denom][1]) * denoms[denom][2] > 0.5:
                 filtered_balances += [entry]
-
-        # print(denoms[denom][0])
-        # print(num_real)
-        # print(total)
-        # print(float(num_real)/float(total))
+            else:
+                unincluded_balances += [entry]
 
         # print (str(denoms[denom][0]) + ' - ' + str(float(len(filtered_balances))/float(total)) + '%')
         print (str(denoms[denom][0]) + ' - ' + denom + ' - ' + str(len(filtered_balances)))
         
 
-        if len(filtered_balances) > 0:
-            with open('filtered/'+ denoms[denom][0] +'_filtered.json', 'w') as f:
-                json.dump(filtered_balances, f, indent=4)
+        # if len(filtered_balances) > 0:
+        #     with open('filtered/'+ denoms[denom][0] +'_filtered.json', 'w') as f:
+        #         json.dump(filtered_balances, f, indent=4)
+
+        # if len(unincluded_balances) > 0:
+        #     with open('unincluded/'+ denoms[denom][0] +'.json', 'w') as f:
+        #         json.dump(unincluded_balances, f, indent=4)
 
     except FileNotFoundError:
         pass
